72b5a8308c76c3622bb5bc8a142c95b28eb450d5
Ryanz Ofc <88297151+ryanzgnzz@users.noreply.github.com>
2023-01-04 05:30:56 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -35,8 +35,8 @@ var port = process.env.PORT || 8080 || 5000 || 3000
         uptime: muptime(process.uptime()),
         speed: `${neww - old}ms`,
         info:{       
-            owner: 'ZeeoneOfc',            
-            apikey: 'Chat Owner: https://wa.me/62887435047326'
+            owner: 'RyanzOfc',            
+            apikey: 'Chat Owner: https://wa.me/6285863725673'
         }
     }
     res.json(status)
